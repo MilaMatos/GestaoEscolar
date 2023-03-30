7915e88981873ae53876ece745a42a9f1fec0b8e
Camila Vanessa <cvanessamatos@gmail.com>
2023-03-30 12:33:29 -0300

    Arquivos bases

---
+++ src/main/java/com/mycompany/sistemaescolar/SistemaEscolar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaescolar;
+
+/**
+ *
+ * @author cvane
+ */
+public class SistemaEscolar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
